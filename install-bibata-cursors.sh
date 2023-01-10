74eed054c1b37c382e9e3a299bd128097ba20ddf
looping13 <olivier.dendayne@gmail.com>
2023-01-10 23:32:59 +0100

    add Bibata cursor install script

---
+++ install-bibata-cursors.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Download from this location
+#https://github.com/ful1e5/Bibata_Cursor/releases
+cd ~/Downloads
+
+#Uninstall previous intll
+#rm ~/.icons/Bibata-*                  # Remove from local users
+sudo rm /usr/share/icons/Bibata-*     # Remove from all users
+
+
+#Install Bibata cursor
+tar -xvf Bibata.tar.gz                # extract `Bibata.tar.gz`
+#mv Bibata-* ~/.icons/                 # Install to local users
+sudo mv Bibata-* /usr/share/icons/    # Install to all users
+
+rm -rf Bibata-*
+
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
