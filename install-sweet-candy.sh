fac97c11483ab70855b54489232d5d6e206ea936
looping13 <olivier.dendayne@gmail.com>
2023-01-15 01:26:44 +0100

    fix links

--- install-sweet-candy.sh
+++ install-sweet-candy.sh
@@ -22,13 +22,20 @@ sudo mv candy* /usr/share/icons/    # Install to all users
 sudo mv Sweet-folders/Sweet-Rainbow /usr/share/icons/ 
 sudo mv Sweet /usr/share/themes/ 
 
+cd /usr/share/icons/candy-icons/apps/scalable
+sudo ln -sf accessories-text-editor.svg org.gnome.TextEditor.svg
+sudo ln -sf meld.svg org.gnome.Meld.svg
+sudo ln -sf org.gnome.Rhythmbox.svg org.gnome.Rhythmbox3.svg
+sudo ln -sf system-software-update.svg software-properties.svg
+
+sudo update-icon-caches /usr/share/icons/candy-icons/
 
 rm -rf ~/Projects/candy-icons
 rm -rf ~/Projects/Sweet*
 
 gsettings set org.gnome.desktop.interface icon-theme Sweet-Rainbow
-#gsettings set org.gnome.desktop.interface gtk-theme "Sweet"
-#gsettings set org.gnome.desktop.wm.preferences theme "Sweet"
+gsettings set org.gnome.desktop.interface gtk-theme Sweet
+gsettings set org.gnome.desktop.wm.preferences theme Sweet
 
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
