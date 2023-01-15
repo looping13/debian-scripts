3b3cb732fd9c26af9857dd31b83a3a49e84b1a6d
looping13 <olivier.dendayne@gmail.com>
2023-01-15 16:00:49 +0100

    add comments to sweet-candy

--- install-sweet-candy.sh
+++ install-sweet-candy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+###################################
+## Review - Modify - Run - Enjoy ##
+###################################
 
+echo "## Cloning github repositories "
 cd ~/Projects/
 rm -rf ~/Projects/candy-icons
 rm -rf ~/Projects/Sweet-folders
@@ -13,26 +17,34 @@ git clone https://github.com/EliverLara/Sweet.git ~/Projects/Sweet
 # sed -i 's/Inherits=/Inherits=candy-icons,/g' ~/Projects/Sweet-folders/Sweet-Rainbow/index.theme
 
 #uninstall previous for all users
+echo "## Uninstalling old files for all users"
 sudo rm -rf /usr/share/icons/candy*
 sudo rm -rf /usr/share/icons/Sweet-Rainbow
 sudo rm -rf /usr/share/themes/Sweet
 
 #install new for all users
+echo "## Installing new files for all users"
 sudo mv candy* /usr/share/icons/    # Install to all users
 sudo mv Sweet-folders/Sweet-Rainbow /usr/share/icons/ 
 sudo mv Sweet /usr/share/themes/ 
 
+# fixing some icons symlink into candy-icons
+echo "## Fixing candy-icons symlinks"
 cd /usr/share/icons/candy-icons/apps/scalable
 sudo ln -sf accessories-text-editor.svg org.gnome.TextEditor.svg
 sudo ln -sf meld.svg org.gnome.Meld.svg
 sudo ln -sf org.gnome.Rhythmbox.svg org.gnome.Rhythmbox3.svg
 sudo ln -sf system-software-update.svg software-properties.svg
-
+# Need to update icon cache after modification of the folder
 sudo update-icon-caches /usr/share/icons/candy-icons/
 
+# Cleaning local repository
+echo "## Cleaning local user temp repositories"
 rm -rf ~/Projects/candy-icons
 rm -rf ~/Projects/Sweet*
 
+# Set themes in Gnome
+echo "## Setting the themes for Gnome"
 gsettings set org.gnome.desktop.interface icon-theme Sweet-Rainbow
 gsettings set org.gnome.desktop.interface gtk-theme Sweet
 gsettings set org.gnome.desktop.wm.preferences theme Sweet
