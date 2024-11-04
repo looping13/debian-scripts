6e3e15bfaac2187db7cf78411cba4f4819b49854
looping <looping13@localhost.local>
2024-11-04 18:48:57 +0100

    btrfs mount options

--- debian_install_btrfs_auto_partition.sh
+++ debian_install_btrfs_auto_partition.sh
@@ -2,6 +2,7 @@
 echo "A script to create btrfs partition - just a guy linux style"
 dev_target=$(df /target | awk 'NR==2 {print $1}')
 dev_efi=$(df /target/boot/efi | awk 'NR==2 {print $1}')
+defaults="noatime,noacl,compress=zstd:5,autodefrag"
 echo "Unmount /target"
 umount /target/boot/efi/
 umount /target/
@@ -20,7 +21,7 @@ btrfs subvolume create @opt
 
 
 echo "Mount /target"
-mount -o defaults,subvol=@ $dev_target /target
+mount -o  $defaults,subvol=@ $dev_target /target
 echo "Make directories"
 mkdir -p /target/boot/efi
 mkdir -p /target/home
@@ -30,11 +31,11 @@ mkdir -p /target/tmp
 mkdir -p /target/opt
 
 echo "Mount subvolumes into /target"
-mount -o defaults,subvol=@home $dev_target /target/home
-mount -o defaults,subvol=@root $dev_target /target/root
-mount -o defaults,subvol=@log $dev_target /target/var/log
-mount -o defaults,subvol=@tmp $dev_target /target/tmp
-mount -o defaults,subvol=@opt $dev_target /target/opt
+mount -o  $defaults,subvol=@home $dev_target /target/home
+mount -o  $defaults,subvol=@root $dev_target /target/root
+mount -o  $defaults,subvol=@log $dev_target /target/var/log
+mount -o  $defaults,subvol=@tmp $dev_target /target/tmp
+mount -o  $defaults,subvol=@opt $dev_target /target/opt
 
 mount $dev_efi /target/boot/efi
 
@@ -43,18 +44,21 @@ sed -i.bak '/rootfs/s/^/# /' /target/etc/fstab
 echo "Find the UUID of disk"
 disk_uuid=$(blkid -o value $dev_target | head -1)
 echo "append mount points into fstab"
-echo "UUID=$disk_uuid / btrfs defaults,subvol=@ 0 0" >> /target/etc/fstab
-echo "UUID=$disk_uuid /home btrfs defaults,subvol=@home 0 0" >> /target/etc/fstab
-echo "UUID=$disk_uuid /root btrfs defaults,subvol=@root 0 0" >> /target/etc/fstab
-echo "UUID=$disk_uuid /var/log btrfs defaults,subvol=@log 0 0" >> /target/etc/fstab
-echo "UUID=$disk_uuid /tmp btrfs defaults,subvol=@tmp 0 0" >> /target/etc/fstab
-echo "UUID=$disk_uuid /opt btrfs defaults,subvol=@opt 0 0" >> /target/etc/fstab
+echo "UUID=$disk_uuid / btrfs  $defaults,subvol=@ 0 0" >> /target/etc/fstab
+echo "UUID=$disk_uuid /home btrfs  $defaults,subvol=@home 0 0" >> /target/etc/fstab
+echo "UUID=$disk_uuid /root btrfs  $defaults,subvol=@root 0 0" >> /target/etc/fstab
+echo "UUID=$disk_uuid /var/log btrfs  $defaults,subvol=@log 0 0" >> /target/etc/fstab
+echo "UUID=$disk_uuid /tmp btrfs  $defaults,subvol=@tmp 0 0" >> /target/etc/fstab
+echo "UUID=$disk_uuid /opt btrfs  $defaults,subvol=@opt 0 0" >> /target/etc/fstab
 
 echo "*****************************************************"
 cat /target/etc/fstab | tail -n 12
 echo "*****************************************************"
 echo "*****************************************************"
 btrfs subvolume list /target
+echo "*****************************************************"
+df -h 
+echo "*****************************************************"
 echo "*****************************************************"
 echo "BTRFS partitioning DONE ! just unmount /mnt to be safe with umount /mnt/"
 echo "then exit and resume installation of base system"
