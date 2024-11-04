cbfa14dba3526968f982cc8531800a9169ee3e14
looping <looping13@localhost.local>
2024-11-04 16:45:35 +0100

    btrfs again almost final

--- debian_install_btrfs_auto_partition.sh
+++ debian_install_btrfs_auto_partition.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-echo "This script to create btrfs partition - just a guy linux style"
+echo "A script to create btrfs partition - just a guy linux style"
 dev_target=$(df /target | awk 'NR==2 {print $1}')
 dev_efi=$(df /target/boot/efi | awk 'NR==2 {print $1}')
-echo "Unmount target "
-umount -v /target/boot/efi/
-umount -v /target/
+echo "Unmount /target"
+umount /target/boot/efi/
+umount /target/
 
-echo "mount disk and create btrfs subvolumes"
-mount -v $dev_target /mnt
+echo "Mount device to /mnt and create btrfs subvolumes"
+mount $dev_target /mnt
 cd /mnt
 mv @rootfs/ @
 btrfs subvolume create @home
@@ -15,28 +15,28 @@ btrfs subvolume create @root
 btrfs subvolume create @log
 btrfs subvolume create @tmp
 btrfs subvolume create @opt
-btrfs subvolume list
-umount -v /mnt/
-umount -v /mnt
+
+# btrfs subvolume list /mnt
+
 
 echo "Mount /target"
-mount -v -o defaults,subvol=@ $dev_target /target
+mount -o defaults,subvol=@ $dev_target /target
 echo "Make directories"
-mkdir -v -p /target/boot/efi
-mkdir -v -p /target/home
-mkdir -v -p /target/root
-mkdir -v -p /target/var/log
-mkdir -v -p /target/tmp
-mkdir -v -p /target/opt
+mkdir -p /target/boot/efi
+mkdir -p /target/home
+mkdir -p /target/root
+mkdir -p /target/var/log
+mkdir -p /target/tmp
+mkdir -p /target/opt
 
-echo "mount subvolumes into /target"
-mount -v -o defaults,subvol=@home $dev_target /target/home
-mount -v -o defaults,subvol=@root $dev_target /target/root
-mount -v -o defaults,subvol=@log $dev_target /target/var/log
-mount -v -o defaults,subvol=@tmp $dev_target /target/tmp
-mount -v -o defaults,subvol=@opt $dev_target /target/opt
+echo "Mount subvolumes into /target"
+mount -o defaults,subvol=@home $dev_target /target/home
+mount -o defaults,subvol=@root $dev_target /target/root
+mount -o defaults,subvol=@log $dev_target /target/var/log
+mount -o defaults,subvol=@tmp $dev_target /target/tmp
+mount -o defaults,subvol=@opt $dev_target /target/opt
 
-mount -v $dev_efi /target/boot/efi
+mount $dev_efi /target/boot/efi
 
 echo " Now change /target/etc/fstab"
 sed -i.bak '/rootfs/s/^/# /' /target/etc/fstab
@@ -50,6 +50,14 @@ echo "UUID=$disk_uuid /var/log btrfs defaults,subvol=@log 0 0" >> /target/etc/fs
 echo "UUID=$disk_uuid /tmp btrfs defaults,subvol=@tmp 0 0" >> /target/etc/fstab
 echo "UUID=$disk_uuid /opt btrfs defaults,subvol=@opt 0 0" >> /target/etc/fstab
 
-
+echo "*****************************************************"
+cat /target/etc/fstab | tail -n 12
+echo "*****************************************************"
+echo "*****************************************************"
+btrfs subvolume list /target
+echo "*****************************************************"
+echo "BTRFS partitioning DONE ! just unmount /mnt to be safe with umount /mnt/"
+echo "then exit and resume installation of base system"
+# umount /mnt/
 
 
