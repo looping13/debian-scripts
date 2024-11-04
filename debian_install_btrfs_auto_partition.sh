167d806cac30fdd819a6da27945a3371b393294d
looping <looping13@localhost.local>
2024-11-04 11:27:06 +0100

    btrfs auto partition

--- debian_install_btrfs_auto_partition.sh
+++ debian_install_btrfs_auto_partition.sh
@@ -1,47 +1,50 @@
 #!/bin/sh
 echo "This script to create btrfs partition - just a guy linux style"
 echo "Unmount target "
-umount /target/boot/efi/
-umount /target/
+umount -v /target/boot/efi/
+umount -v /target/
 
 echo "mount disk and create btrfs subvolumes"
-mount /dev/sda2 /mnt
+mount -v /dev/sda2 /mnt
 cd /mnt
 mv @rootfs/ @
-btrfs su cr @home
-btrfs su cr @root
-btrfs su cr @log
-btrfs su cr @tmp
-btrfs su cr @opt
+btrfs subvolume create @home
+btrfs subvolume create @root
+btrfs subvolume create @log
+btrfs subvolume create @tmp
+btrfs subvolume create @opt
+btrfs subvolume list
+umount -v /mnt/
+umount -v /mnt
 
 echo "Mount /target"
-mount -o noatime,compress=zstd,subvol=@ /dev/sda2 /target
+mount -v -o defaults,subvol=@ /dev/sda2 /target
 echo "Make directories"
-mkdir -p /target/boot/efi
-mkdir -p /target/home
-mkdir -p /target/root
-mkdir -p /target/var/log
-mkdir -p /target/tmp
-mkdir -p /target/opt
+mkdir -v -p /target/boot/efi
+mkdir -v -p /target/home
+mkdir -v -p /target/root
+mkdir -v -p /target/var/log
+mkdir -v -p /target/tmp
+mkdir -v -p /target/opt
 
 echo "mount subvolumes into /target"
-mount -o noatime,compress=zstd,subvol=@home /dev/sda2 /target/home
-mount -o noatime,compress=zstd,subvol=@root /dev/sda2 /target/root
-mount -o noatime,compress=zstd,subvol=@log /dev/sda2 /target/var/log
-mount -o noatime,compress=zstd,subvol=@tmp /dev/sda2 /target/tmp
-mount -o noatime,compress=zstd,subvol=@opt /dev/sda2 /target/opt
+mount -v -o defaults,subvol=@home /dev/sda2 /target/home
+mount -v -o defaults,subvol=@root /dev/sda2 /target/root
+mount -v -o defaults,subvol=@log /dev/sda2 /target/var/log
+mount -v -o defaults,subvol=@tmp /dev/sda2 /target/tmp
+mount -v -o defaults,subvol=@opt /dev/sda2 /target/opt
 
-mount /dev/sda1 /target/boot/efi
+mount -v /dev/sda1 /target/boot/efi
 
 echo " Now change /target/etc/fstab"
 echo "Find the uuid of disk"
 disk_uuid=$(blkid -o value /dev/sda2 | head -1)
 echo "append mount points into fstab"
-echo "uuid=$disk_uuid / btrfs noatime,compress=zstd,subvol=@ 0 0" >> /target/etc/fstab
-echo "uuid=$disk_uuid /home btrfs noatime,compress=zstd,subvol=@home 0 0" >> /target/etc/fstab
-echo "uuid=$disk_uuid /root btrfs noatime,compress=zstd,subvol=@root 0 0" >> /target/etc/fstab
-echo "uuid=$disk_uuid /var/log btrfs noatime,compress=zstd,subvol=@log 0 0" >> /target/etc/fstab
-echo "uuid=$disk_uuid /tmp btrfs noatime,compress=zstd,subvol=@tmmp 0 0" >> /target/etc/fstab
-echo "uuid=$disk_uuid /opt btrfs noatime,compress=zstd,subvol=@opt 0 0" >> /target/etc/fstab
+echo "uuid=$disk_uuid / btrfs defaults,subvol=@ 0 0" >> /target/etc/fstab
+echo "uuid=$disk_uuid /home btrfs defaults,subvol=@home 0 0" >> /target/etc/fstab
+echo "uuid=$disk_uuid /root btrfs defaults,subvol=@root 0 0" >> /target/etc/fstab
+echo "uuid=$disk_uuid /var/log btrfs defaults,subvol=@log 0 0" >> /target/etc/fstab
+echo "uuid=$disk_uuid /tmp btrfs defaults,subvol=@tmp 0 0" >> /target/etc/fstab
+echo "uuid=$disk_uuid /opt btrfs defaults,subvol=@opt 0 0" >> /target/etc/fstab
 
 
