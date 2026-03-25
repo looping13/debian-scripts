5a32f6958955e62dc81248c316668c6d920d3ae0
looping <looping13@localhost.local>
2026-03-25 13:22:25 +0100

    remove snapshot in btrfs script, handled by snapper install

--- debian_install_btrfs_auto_partition.sh
+++ debian_install_btrfs_auto_partition.sh
@@ -26,11 +26,11 @@ mount $dev_target /mnt
 cd /mnt
 mv @rootfs/ @
 btrfs subvolume create @home
-btrfs subvolume create @root
+btrfs subvolume create @cache
 btrfs subvolume create @log
 btrfs subvolume create @tmp
 btrfs subvolume create @opt
-btrfs subvolume create @snapshots
+#btrfs subvolume create @snapshots
 
 # btrfs subvolume list /mnt
 
@@ -40,19 +40,19 @@ mount -o $mount_options_btrfs,subvol=@ $dev_target /target
 echo "Make directories"
 mkdir -p /target/boot/efi
 mkdir -p /target/home
-mkdir -p /target/root
+mkdir -p /target/var/cache
 mkdir -p /target/var/log
 mkdir -p /target/tmp
 mkdir -p /target/opt
-mkdir -p /target/.snapshots
+#mkdir -p /target/.snapshots
 
 echo "Mount subvolumes into /target"
 mount -o $mount_options_btrfs,subvol=@home $dev_target /target/home
-mount -o $mount_options_btrfs,subvol=@root $dev_target /target/root
+mount -o $mount_options_btrfs,subvol=@cache $dev_target /target/var/cache
 mount -o $mount_options_btrfs,subvol=@log $dev_target /target/var/log
 mount -o $mount_options_btrfs,subvol=@tmp $dev_target /target/tmp
 mount -o $mount_options_btrfs,subvol=@opt $dev_target /target/opt
-mount -o $mount_options_btrfs,subvol=@snapshots $dev_target /target/.snapshots
+#mount -o $mount_options_btrfs,subvol=@snapshots $dev_target /target/.snapshots
 
 mount $dev_efi /target/boot/efi
 
@@ -64,11 +64,11 @@ disk_uuid=$(blkid -o value $dev_target | head -1)
 echo "append mount points into fstab"
 echo "UUID=$disk_uuid / btrfs $mount_options_btrfs,subvol=@ 0 0" >> /target/etc/fstab
 echo "UUID=$disk_uuid /home btrfs $mount_options_btrfs,subvol=@home 0 0" >> /target/etc/fstab
-echo "UUID=$disk_uuid /root btrfs $mount_options_btrfs,subvol=@root 0 0" >> /target/etc/fstab
+echo "UUID=$disk_uuid /var/cache btrfs $mount_options_btrfs,subvol=@cache 0 0" >> /target/etc/fstab
 echo "UUID=$disk_uuid /var/log btrfs $mount_options_btrfs,subvol=@log 0 0" >> /target/etc/fstab
 echo "UUID=$disk_uuid /tmp btrfs $mount_options_btrfs,subvol=@tmp 0 0" >> /target/etc/fstab
 echo "UUID=$disk_uuid /opt btrfs $mount_options_btrfs,subvol=@opt 0 0" >> /target/etc/fstab
-echo "UUID=$disk_uuid /.snapshots btrfs $mount_options_btrfs,subvol=@snapshots 0 0" >> /target/etc/fstab
+#echo "UUID=$disk_uuid /.snapshots btrfs $mount_options_btrfs,subvol=@snapshots 0 0" >> /target/etc/fstab
 
 echo "*****************************************************"
 cat /target/etc/fstab | tail -n 12
